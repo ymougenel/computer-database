9db736aecf6b1b7c1614dd90856651243c14a6fe
yann mougenel <ymougenel@gmail.com>
2016-04-28 12:00:58 +0200

    [Web ui] : Selenium integration

---
+++ database/src/test/java/database/selenium/ComputerInsertionTest.java
@@ -0,0 +1,50 @@
+package database.selenium;
+
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ComputerInsertionTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "http://localhost:8080/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testComputerInsertion() throws Exception {
+        driver.get(baseUrl + "/database/dashboard");
+        driver.findElement(By.linkText("1")).click();
+        driver.findElement(By.id("addComputer")).click();
+        driver.findElement(By.id("computerName")).clear();
+        driver.findElement(By.id("computerName")).sendKeys("Test");
+        driver.findElement(By.id("introduced")).clear();
+        driver.findElement(By.id("introduced")).sendKeys("2012-02-04");
+        driver.findElement(By.id("discontinued")).clear();
+        driver.findElement(By.id("discontinued")).sendKeys("2013-02-05");
+        new Select(driver.findElement(By.id("companyId"))).selectByVisibleText("Apple Inc.");
+        driver.findElement(By.cssSelector("input.btn.btn-primary")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+}
---
+++ database/src/test/java/database/selenium/PaginationExplorerTest.java
@@ -0,0 +1,46 @@
+package database.selenium;
+
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class PaginationExplorerTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "http://localhost:8080/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testPaginationExploration() throws Exception {
+        driver.get(baseUrl + "/database/dashboard?pageIndex=5");
+        driver.findElement(By.linkText("3")).click();
+        driver.findElement(By.id("addComputer")).click();
+        driver.findElement(By.linkText("Cancel")).click();
+        driver.findElement(By.linkText("4")).click();
+        driver.findElement(By.xpath("//li[9]/a/span")).click();
+        driver.findElement(By.linkText("«")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+}
